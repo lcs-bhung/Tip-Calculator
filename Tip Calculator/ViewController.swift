eac34775680408e1b6f4c23948fbf12e8a6f5270
Branton Hung <BHung@lcs.on.ca>
2019-10-10 11:09:46 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Branton Hung on 2019-10-10.
+//  Copyright © 2019 Branton Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
