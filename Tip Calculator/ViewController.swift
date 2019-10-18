a416ed21a9b3a513b5fb9b4f751fd34283bc2f83
Branton Hung <BHung@lcs.on.ca>
2019-10-18 11:07:57 -0400

    Fixed the brackets

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Mark: Properties
+    @IBOutlet weak var theBill: UITextField!
+    
 
+    @IBOutlet weak var tip: UITextField!
+    
+    @IBOutlet weak var numberOfPeople: UITextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var tipPerPersonLabel: UILabel!
 
-
+    //Calculate
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        //Get the user imput
+        let billAsString  = theBill
+        let tipPercentAsString = tip
+        let noPeopleAsString = numberOfPeople
+        
+        //Convert to double
+        let bill = Double(billAsString)
+        let tipPercent = Double(tipPercentAsString)
+        let noPeople = Double(noPeopleAsString)
+        
+        
+            let totalTip = bill * tipPercent
+            let tipPerPerson = totalTip / noPeople
+    }
+    
 }
 
